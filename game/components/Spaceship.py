8450eebc71be57ffba638b2453bb6590e3d6ef78
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-07-14 16:02:20 -0500

    Tarea 1
    Agregar funcionalidad para que la nave se transporte al lado opuesto de la pantalla al alcanzar el lado izquierdo o derecho.

--- game/components/Spaceship.py
+++ game/components/Spaceship.py
@@ -16,24 +16,37 @@ class Spaceship:
         self.rect.x = self.X_POS
         self.rect.y = self.Y_POS
 
-    def update(self, game_speed,user_input):
+    def update(self, game_speed, user_input):
         if user_input[pygame.K_LEFT]:
             self.move_left(game_speed)
 
+        if user_input[pygame.K_RIGHT]:
+            self.move_rigth(game_speed)
+
         if user_input[pygame.K_UP]:
             self.move_up(game_speed)
+
+        if user_input[pygame.K_DOWN]:
+            self.move_down(game_speed)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
     def move_left(self, game_speed):
         self.rect.x -= game_speed
+        if self.rect.x <= 0:
+            self.rect.x = (SCREEN_WIDTH - self.WIDTH)
 
-    def move_left(self, game_speed):
+    def move_rigth(self, game_speed):
         self.rect.x += game_speed
+        if self.rect.x >= (SCREEN_WIDTH - self.WIDTH):
+            self.rect.x = 0
+        
 
     def move_up(self, game_speed):
-        self.rect.y -= game_speed
+        if self.rect.y >= 0:
+            self.rect.y -= game_speed
 
     def move_down(self, game_speed):
-        self.rect.y += game_speed
+        if self.rect.y <= (SCREEN_HEIGHT - self.HEIGTH):
+            self.rect.y += game_speed
